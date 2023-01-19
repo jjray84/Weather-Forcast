c3f09ca0ee945cfac5cf772174e1b0565bca8a81
Jonathan Ray <jonathanjudsonray@gmail.com>
2023-01-19 16:00:55 -0500

    Created functions for clicking the search button, current weather, and future weather and confirmed that the functions all work

--- assests/script.js
+++ assests/script.js
@@ -1,22 +1,42 @@
-var searchButton = document.querySelector('#search-button'); 
-var APIKey = '6a52459f2c446b85f53fd4ca81881f54';
-var city;
-var queryURL = "api.openweathermap.org/data/2.5/forecast?q=cityname&appid" + APIKey;
+const searchButton = document.querySelector('#search-button'); 
+const APIKey = '6a52459f2c446b85f53fd4ca81881f54';
+const futureWeather = "api.openweathermap.org/data/2.5/forecast?q=cityname&appid";
+const currentWeather = "https://api.openweathermap.org/data/2.5/weather?q=CITYNAME&appid="
 
-searchButton.onclick = function() {
+searchButton.onclick = function(event) {
+    event.preventDefault();
     console.log('You clicked me');
+    userInput();
+};
+
+function userInput(event) {
+    let city = searchBar.value;
+    console.log(city);
+    if (userInput == null) {
+        return;
+    } else {
+        currentCityWeather(city);
+        futureCityWeather(city);
+    }
+};
+
+function currentCityWeather(city) {
+    fetch ("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey  + "&units=imperial").then(function(response){
+       return response.json();
+    }).then(function(data) {
+        console.log(data);
+    }); 
+        
+}
+
+function futureCityWeather(city) {
+    fetch ("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey + "&units=imperial").then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        console.log(data);
+    });
 };
 
 
 
-
-searchButton.addEventListener('click', console.log(searchButton, "clicked"));
-
-// get user input
-
-// fetch data
-
-// display data (innerHTML????)
-
 // display under the search bar the previous cities searched
-
